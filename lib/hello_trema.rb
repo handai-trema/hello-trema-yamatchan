d4f713725a0ca9d523a445754825dd2d8030f454
yamatchan <u.ma.ymd@gmail.com>
2016-10-05 16:11:10 +0900

    レポート課題(16.10.05)完了

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,14 @@
 # Hello World!
 class HelloTrema < Trema::Controller
   def start(args)
-    logger.info "Trema started (args = #{args.inspect})."
+    logger.info "#{self.name} started (args = #{args.inspect})."
   end
 
   def switch_ready(datapath_id)
     logger.info format('Hello %#x!', datapath_id)
+  end
+
+  def switch_disconnected(datapath_id)
+    logger.info "Bye #{datapath_id.to_hex}"
   end
 end
